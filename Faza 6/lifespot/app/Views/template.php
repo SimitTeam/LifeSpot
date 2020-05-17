643a85f004a967f5f53a30f136b6064593651ace
Edvvin <edvvin@mail.com>
2020-05-17 22:20:19 +0200

    Template changed -userType in ViewConfig should now be User, Moderator, Admin

--- Faza 6/lifespot/app/Views/template.php
+++ Faza 6/lifespot/app/Views/template.php
@@ -12,22 +12,29 @@
 		<link rel="stylesheet" href=" <?= site_url("./assets/bootstrap-4.4.1-dist/css/bootstrap.css") ?> ">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
 		<link rel="stylesheet" href=" <?= site_url("./assets/css/style.css") ?>">
+		<link rel="stylesheet" type="text/css" href="<?=site_url("./assets/DataTables/datatables.min.css")?>"/>
 
 		<script src="<?= site_url("./assets/js/jquery-3.4.1.min.js") ?> "></script>
 		<script src="<?= site_url("./assets/bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js") ?> "></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 		<link rel="shortcut icon" href="<?= site_url("./assets/img/logo.png") ?> "/>
+		<script type="text/javascript" src="<?=site_url("./assets/DataTables/datatables.min.js")?>"></script>
+		<script>
+		$(document).ready(function() {
+			$('#example').DataTable();
+		} );
+		</script>
 
 	</head>
 	<body>
 	<?php
-		if(!strcmp($config->userType,"admin")){
+		if(!strcmp($config->userType,"Admin")){
 			echo($this-> include('headers/adminheader'));
 		}
-		else if(!strcmp($config->userType,"moderator")){
+		else if(!strcmp($config->userType,"Moderator")){
 			echo($this-> include('headers/moderatorheader'));
 		}
-		else if(!strcmp($config->userType,"user")){
+		else if(!strcmp($config->userType,"User")){
 			echo($this-> include('headers/userheader'));
 		}
 		else{
